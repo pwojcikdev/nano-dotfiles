be238ac7db6f5241705b5c874a516568ea44eee6
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2023-02-24 16:15:54 +0100

    build node with environment

--- build-node.sh
+++ build-node.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 NAME=${1:-nano-node}
+ENV_NAME=${2:-nano-env}
+COMPILER=${3:-gcc}
 
-docker build -f docker/node/Dockerfile -t $NAME .
+echo "NAME: $NAME"
+echo "ENV_NAME: $ENV_NAME"
+echo "COMPILER: $COMPILER"
+
+# build environment image
+docker build -f docker/ci/Dockerfile-base -t $ENV_NAME:base .
+docker build --build-arg ENV_REPOSITORY=$ENV_NAME -f docker/ci/Dockerfile-$COMPILER -t $ENV_NAME:$COMPILER .
+
+# build node image
+docker build --build-arg ENV_REPOSITORY=$ENV_NAME --build-arg COMPILER=$COMPILER -f docker/node/Dockerfile -t $NAME .
